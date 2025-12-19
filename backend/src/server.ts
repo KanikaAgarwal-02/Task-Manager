997fb15d2a36beba5ce5aaa7a202b3c16c79f327
Kanika Agarwal <kanikaagrwl882@gmail.com>
2025-12-19 20:17:06 +0530

    Fix CORS for Vercel Frontend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -20,6 +20,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
+      "https://task-manager-virid-kappa-44.vercel.app",
     ],
     credentials: true,
   })
