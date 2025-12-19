15a7a6e59bf3b7e5fab3ae214c986a81ed8ba336
Kanika Agarwal <kanikaagrwl882@gmail.com>
2025-12-19 20:13:56 +0530

    Fix socket.io backend URL

--- frontend/src/socket/socket.ts
+++ frontend/src/socket/socket.ts
@@ -1,7 +1,11 @@
 import { io } from "socket.io-client";
 
-const socket = io(import.meta.env.VITE_SOCKET_URL, {
-  withCredentials: true,
-});
+const socket = io(
+  "https://task-manager-backend-lef3.onrender.com",
+  {
+    withCredentials: true,
+    transports: ["websocket"],
+  }
+);
 
 export default socket;
