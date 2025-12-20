f167e2a1e8a62b3739ad6595fcf8f2c7d61af589
Kanika Agarwal <kanikaagrwl882@gmail.com>
2025-12-20 14:16:58 +0530

    Upadte

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,18 +1,31 @@
-import mongoose, { Schema, Document } from "mongoose";
+// import mongoose, { Schema, Document } from "mongoose";
 
-export interface IUser extends Document {
-  name: string;
-  email: string;
-  password: string;
-}
+// export interface IUser extends Document {
+//   name: string;
+//   email: string;
+//   password: string;
+// }
 
-const UserSchema: Schema<IUser> = new Schema(
+// const UserSchema: Schema<IUser> = new Schema(
+//   {
+//     name: { type: String, required: true },
+//     email: { type: String, required: true, unique: true },
+//     password: { type: String, required: true }
+//   },
+//   { timestamps: true }
+// );
+
+// export default mongoose.model<IUser>("User", UserSchema);
+
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
   },
   { timestamps: true }
 );
 
-export default mongoose.model<IUser>("User", UserSchema);
+export default model("User", userSchema);
